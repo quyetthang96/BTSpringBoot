65e5648a3f6227b95e480d49bc70b529993badd3
QuyetThang <quyetthang326@gmail.com>
2022-04-08 00:37:30 +0700

    start

---
+++ manege_user/src/main/java/com/example/manege_user/ManegeUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.manege_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManegeUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManegeUserApplication.class, args);
+	}
+
+}
